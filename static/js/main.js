c5b1d7e962b6f2df3d9f149ef4ffecca1b470cb6
wojlin <wojciech.linowski.2137@gmail.com>
2024-06-18 09:36:04 +0200

    fixed paralax

--- static/js/main.js
+++ static/js/main.js
@@ -11,7 +11,7 @@ class Main
        }, 2000); 
 
        setTimeout(function(){
-        main.paralax.scrollToEnd();
+            main.paralax.scrollToEnd();
         }, 6000);
 
         setTimeout(function(){
@@ -48,7 +48,7 @@ class Paralax
         for(let i = 1; i < 10; i++)
         {
             this.panels.push(document.getElementById("paralax-" + i.toString()));
-        }
+        }     
     }
 
     scrollToStart()
@@ -58,37 +58,37 @@ class Paralax
 
     scrollToMiddle()
     {
-        this.panels[2].style.top = "1vh";
-        this.panels[3].style.top = "-3vh";
-        this.panels[4].style.top = "-5vh";
-        this.panels[5].style.top = "-10vh";
-        this.panels[6].style.top = "-5vh";
-        this.panels[7].style.top = "-2vh";
-        this.panels[8].style.height = "50vh";
+        this.panels[2].style.bottom = "45vh";
+        this.panels[3].style.top = "37vh";
+        this.panels[4].style.top = "34vh";
+        this.panels[5].style.top = "25vh";
+        this.panels[6].style.top = "18vh";
+        this.panels[7].style.top = "15vh";
+        this.panels[8].style.height = "40vh";
     }
 
     scrollToEnd()
     {
-        this.panels[2].style.top = "5vh";
-        this.panels[3].style.top = "-3vh";
-        this.panels[4].style.top = "-12vh";
-        this.panels[5].style.top = "-25vh";
-        this.panels[6].style.top = "-15vh";
-        this.panels[7].style.top = "-13vh";
+        this.panels[2].style.bottom = "42vh";
+        this.panels[3].style.top = "35vh";
+        this.panels[4].style.top = "28vh";
+        this.panels[5].style.top = "18vh";
+        this.panels[6].style.top = "5vh";
+        this.panels[7].style.top = "5vh";
         this.panels[8].style.height = "30vh";
     }
 
     scrollToResult()
     {
-        this.panels[0].style.top = "-10vh";
-        this.panels[0].style.height = "100vh";
-        this.panels[1].style.top = "-100vh";
-        this.panels[2].style.top = "-90vh";
-        this.panels[3].style.top = "-80vh";
-        this.panels[4].style.top = "-80vh";
-        this.panels[5].style.top = "-80vh";
-        this.panels[6].style.top = "-80vh";
-        this.panels[7].style.top = "-80vh";
+        this.panels[0].style.height = "120vh";
+        this.panels[0].style.top = "-20vh";
+        this.panels[1].style.top = "-30vh";
+        this.panels[2].style.bottom = "130vh";
+        this.panels[3].style.top = "-90vh";
+        this.panels[4].style.top = "-90vh";
+        this.panels[5].style.top = "-90vh";
+        this.panels[6].style.top = "-90vh";
+        this.panels[7].style.top = "-50vh";
         this.panels[8].style.height = "0vh";
 
         setTimeout(function()
@@ -130,3 +130,72 @@ class Point
 
 
 var main = new Main();
+
+
+
+
+
+
+
+
+
+
+function a() {
+    const bg = document.querySelector("#bg");
+    const base = document.querySelector("#base");
+    const reflect = document.querySelector("#reflect");
+    let flg, flg_now;
+    const root = document.documentElement;
+    //let gap = parseInt(getComputedStyle(root).getPropertyValue("--gap"));
+  
+    function pointer(e) {
+        let value = 45;
+        flg = flg_now;
+
+        let css_ratio = 100 - value;
+        root.style.setProperty("--ratio", css_ratio + "vh");
+        if (value == 50) {
+          flg_now = true;
+          base.style.setProperty("background-position", "center");
+          reflect.style.setProperty("background-position", "center");
+        } else if (value > 50) {
+          flg_now = true;
+          base.style.setProperty(
+            "background-position",
+            "top calc(50% + ( var(--ratio) - 50vh ) * 2) center"
+          );
+          reflect.style.setProperty("background-position", "center");
+        } else {
+          flg_now = false;
+          base.style.setProperty("background-position", "center");
+          reflect.style.setProperty(
+            "background-position",
+            "bottom calc(50% + ( var(--ratio) - 50vh ) * 2) center"
+          );
+        }
+    
+        //
+        if (flg != flg_now) {
+          //let filter = "";
+          if (value < 50) {
+            bg.style.setProperty("transform", "scale3d(1, 1, 1)");
+    
+            //bg.style.setProperty("filter", filter);
+          } else {
+            bg.style.setProperty("transform", "scale3d(1, -1, 1)");
+            /*
+            filter = getComputedStyle(reflect).getPropertyValue("--filter");
+    
+            bg.style.setProperty("filter", filter);
+            alert(String(filter));
+    */
+          }
+        }
+      }
+  
+    window.addEventListener("load", pointer);
+  };
+
+  a();
+  
+  
